61df6f2cdff14c752ff509c33c6c72e132dbe3de
你的用户名 <>
2026-01-01 16:17:22 +0800

    awa

---
+++ src/lyl/Example.java
@@ -0,0 +1,16 @@
+package lyl;
+
+import arc.util.Log;
+import mindustry.mod.Mod;
+
+public class Example extends Mod{
+
+    public Example() {
+
+    }
+    @Override
+    public void loadContent(){
+        Log.info("Loading some example content.");
+    }
+
+}
